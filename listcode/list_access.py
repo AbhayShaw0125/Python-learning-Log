f3696e03b1c7aa41092df3cb8c317c3fb1954b83
AbhayShaw0125 <abhayshaw0125@gmail.com>
2025-10-18 16:59:34 +0530

    add: list code

---
+++ listcode/list_access.py
@@ -0,0 +1,12 @@
+labour=["Ram","Sham","Ap","Mam",200,300,400,400,500]
+## so suppose you want to replace or change the name with each name having u at end
+print(labour)
+labour[0:4]=["Ramu","Shamu","Apu","Mamu"]
+print(labour)
+
+
+
+### split method
+hash_code="xyz-abc-123-obj"
+val=hash_code.split("-")
+print(val)
