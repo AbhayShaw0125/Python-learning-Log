a2da46f858a44fcb06b978b247bd3e67712f9575
AbhayShaw0125 <abhayshaw0125@gmail.com>
2025-10-18 18:18:21 +0530

    add: solve star pattern1

---
+++ listcode/star_patter1.py
@@ -0,0 +1,12 @@
+# ## take user input
+# n=int(input("Enter the range \n"))
+# for i in range(0,n):
+#     for j in range(0,i+1):
+#         print("*",end="")
+#     print("\n")
+
+##
+# Easier method to print above pattern
+
+for i in range(5):
+    print(i*"*")
